0ebae47411179e1fd42edda5c30d3f821230289a
jbrennan-0422 <wild.bananas@icloud.com>
2023-01-04 23:40:16 -0500

    semicolon;

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
-const net = require("net")
+const net = require("net");
 
-const server = new net.Server()
-server.listen({ host: "127.0.0.1", port: 9999 })
+const server = new net.Server();
+server.listen({ host: "127.0.0.1", port: 9999 });
 server.on("connection", client => {
   client.write("Hello\n")
-})
+});
