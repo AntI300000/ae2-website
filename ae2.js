492bcfce2aff7dc6ae1fb6fb50e551c5df828e41
denshark03 <121916700+denshark03@users.noreply.github.com>
2023-01-07 22:33:27 -0500

    Add files via upload

--- ae2.js
+++ ae2.js
@@ -1,10 +1,11 @@
 var tableCurrent = 0;
 function createStorageScreen(tableLength) {
 //todo: learn to import the table instead of the number given
+//also parse
 	var container = document.getElementById("storageBox");
 	for(let i = tableCurrent; i < tableLength; i++) {
 		container.insertAdjacentHTML("beforeend","<div> amorgus "+tableCurrent+"</div>");
 		tableCurrent++;
 	}
-	//document.getElementById("summonButton").setAttributeNode(hidden);
+	document.getElementById("summonButton").style.display="none";
 }
